75fd5c7f6b6d182c33128365c6f29e5d8a46047f
foxbj1 <foxbj1@LAPTOPUbuntu>
2020-12-29 15:07:17 -0500

    second commit  Please enter the commit message for your changes. Lines starting

--- hello.c
+++ hello.c
@@ -1,8 +1,8 @@
 
-#include <stdio.h>
+#include <stdio.h>	/* "include" is a search command, just like the "/" command. # is like the * command but causes the program to search backwards for a specif			ied word; because this is the start of the program there is nothing to find but the kernigan book stated that these codes were pulled from larg			er programs. "<studio.h>" is a file searched for.*/ 
 
-int main()
-{
-	printf("hello world\n");
-	return 0;
-}
+int main()				/* int is a command declaring the value of "main()" to be an intager.*/
+{					/*first bracket. main loop commands will be contained within these two brackets*/
+	printf("hello world\n");	/*"ptintf() is a command that will return the value inside the parenthsis, "hello world." "n" is the count of the number of time 					this is repeated. */
+	return 0;	/*"return" is a command that tells the program to return the value of the function when the condition, "0," is met. ";" ends the loop*/
+}			/*closes brackets*/
